cb691d32bcb68e3c091d8555ac8cd0fd55d3e5a9
Tristan Maindron <tmaindron@gmail.com>
2024-04-16 11:36:40 +0200

    Optimizing framerate

--- Level.gd
+++ Level.gd
@@ -8,6 +8,7 @@ var time = 0.0;
 var lost_reason
 
 var game_state
+var hero
 
 func _ready():
 	time = level_time
@@ -21,14 +22,14 @@ func _ready():
 		instance.position = coords
 		instance.tilemap = self
 
+	hero = get_node("/root/Game/Hero");
+
 func player_moved():
 	game_state = GlobalState.GameState.STARTED
 
 func _process(delta):
 	if game_state in [GlobalState.GameState.LOST, GlobalState.GameState.WON, GlobalState.GameState.IDLE]:
 		return
-
-	var hero = get_node("/root/Game/Hero");
 
 	if !hero.alive:
 		lost_reason = hero.death_reason
@@ -46,7 +47,7 @@ func _process(delta):
 
 		get_tree().call_group("ritual", "ritual")
 
-	if len(self.get_used_cells(1)) == 0:
+	if game_state == GlobalState.GameState.STARTED and len(self.get_used_cells(1)) == 0:
 		game_state = GlobalState.GameState.WON
 		get_tree().call_group("player_moved", "level_ended")
 		get_node("/root/Game/Hero").win()
